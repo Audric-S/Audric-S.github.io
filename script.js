873cbfc73972e003f07c4df50a278b3c6e5bfecf
Audric-S <sabatieraudric@gmail.com>
2024-06-09 21:45:26 +0200

    :lipstick: add skills sections and block sidebar

--- script.js
+++ script.js
@@ -12,19 +12,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-
 window.addEventListener('scroll', function () {
     var sidebar = document.getElementById('sidebar');
-    var layoutSections = document.querySelectorAll('.layout > .content > div');
+    var sidebarContainer = document.getElementById('sidebar-container');
+    var layoutSections = document.querySelectorAll('.layout > .content > section');
     var layoutSection = document.querySelector('.layout');
+    var elementHeight = layoutSection.offsetHeight;
+    var layoutSectionBottom = layoutSection.offsetTop + elementHeight;
 
-    var offset = 50;
+    var offset = 100;
 
     if (window.scrollY > layoutSection.offsetTop - offset) {
-        sidebar.classList.add('fixed');
+        if(this.window.scrollY >= layoutSectionBottom - 400){
+          sidebarContainer.classList.add('sidebar-container-bottom');
+          sidebar.classList.remove('fixed');
+        }
+        else{
+            sidebarContainer.classList.remove('sidebar-container-bottom');
+            sidebar.classList.add('fixed');
+        }
     } else {
         sidebar.classList.remove('fixed');
     }
+
 
     layoutSections.forEach(function (section, index) {
         var sectionId = section.getAttribute('id');
@@ -41,6 +51,33 @@ window.addEventListener('scroll', function () {
     });
 
 
+});
+
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 101,
+    modifier: 2,
+    slideShadows: true
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  spaceBetween: 60,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
 });
 
 
